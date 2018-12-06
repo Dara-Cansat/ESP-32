be7a0d2a24b9bef91b19714896fba6babcd4f87c
gotzaza404 <gotzaza404@gmail.com>
2018-12-06 21:17:01 +0700

    Add Gps code

--- Esp32-All/src/main.cpp
+++ Esp32-All/src/main.cpp
@@ -3,13 +3,19 @@
 
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
+static const uint32_t GPSBaud = 9600;
 
-
+#include <TinyGPS++.h>
+TinyGPSPlus gps;
+// HardwareSerial mySerial1(2);
+void processGps();
+void bme280print();
 Adafruit_BMP280 bmp; // I2C
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
+  Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, 16 /*rx*/, 17/* tx */);
+  
 
   if (!bmp.begin()) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
@@ -25,7 +31,38 @@ void setup() {
 }
 
 void loop() {
-    Serial.print(F("Temperature = "));
+    processGps();
+    bme280print();
+    
+}
+
+void processGps() {
+  while (Serial1.available() > 0) {
+    char c = Serial1.read();
+    if (gps.encode(c)) {
+      if (gps.location.isValid()) {
+        Serial.print(gps.location.lat(), 6);
+        Serial.print(F(","));
+        Serial.print(gps.location.lng(), 6);
+        RawDegrees lat = gps.location.rawLat();
+        RawDegrees lng = gps.location.rawLng();
+        char latC[20];
+        char lngC[20];
+        sprintf(latC, "%c%u.%lu", lat.negative?'-':' ', lat.deg, lat.billionths);
+        sprintf(lngC, "%c%u.%lu", lng.negative?'-':' ', lng.deg, lng.billionths);
+        Serial.printf("%s,%s\r\n", latC, lngC);
+      }
+      else
+      {
+        // Serial.println(F("INVALID"));
+      }
+    }
+  }
+}
+
+
+void bme280print(){
+   Serial.print(F("Temperature = "));
     Serial.print(bmp.readTemperature());
     Serial.println(" *C");
 
@@ -36,7 +73,11 @@ void loop() {
     Serial.print(F("Approx altitude = "));
     Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
     Serial.println(" m");
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
 
     Serial.println();
+    
     delay(2000);
 }
